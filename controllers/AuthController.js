c14eb59dd90989cbe4563b0e23edff14ad4a0a49
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-27 12:35:54 -0300

    logout

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -67,4 +67,9 @@ module.exports = class AuthController {
             });
         }
     }
+
+    static logout(req, res) {
+        req.session.destroy()
+        res.redirect('/login')
+    }
 };
