2dfd8175edfb06b300a0354f2aa0961da8a76f6c
mikek182 <marcosvinicius@geekwork.com.br>
2025-05-27 12:37:48 -0300

    login post

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -6,6 +6,40 @@ module.exports = class AuthController {
         res.render('auth/login');
     }
 
+    static async loginPost(req, res) {
+        const { email, password } = req.body;
+
+        // verificar se o usuário existe
+        const user = await User.findOne({ where: { email } });
+
+        if (!user) {
+            req.flash('message', 'Usuário não encontrado! Tente novamente.');
+            return res.render('auth/login', {
+                message: req.flash('message'),
+                email,
+            });
+        }
+
+        // verificar se a senha está correta
+        const passwordMatch = await bcrypt.compare(password, user.password);
+
+        if (!passwordMatch) {
+            req.flash('message', 'Senha inválida! Tente novamente.');
+                return res.render('auth/login', {
+                message: req.flash('message'),
+                email,
+            });
+        }
+
+        // inicializar a sessão
+        req.session.userid = user.id;
+        req.flash('success', 'Login realizado com sucesso!');
+
+        req.session.save(() => {
+            res.redirect('/');
+        });
+    }
+
     static register(req, res) {
         res.render('auth/register');
     }
