f2582956c4906264be6504e098aeb485472c00b3
mikek182 <marcosvinicius@geekwork.com.br>
2025-06-02 17:16:03 -0300

    correção do redirecionamento

--- helpers/auth.js
+++ helpers/auth.js
@@ -1,8 +1,9 @@
 module.exports.checkAuth = function(req, res, next) {
     const userId = req.session.userId
+    
 
     if (!userId) {
-        req.redirect('/login')
+        res.redirect('/login')
     }
 
     next()
--- routes/reminderRoutes.js
+++ routes/reminderRoutes.js
@@ -3,7 +3,10 @@ const router = express.Router();
 
 const ReminderController = require('../controllers/ReminderController');
 
-router.get('/dashboard', ReminderController.dashboard);
-router.get('/', ReminderController.showReminders);
+// helper
+const checkAuth = require('../helpers/auth').checkAuth
+
+router.get('/dashboard', checkAuth, ReminderController.dashboard);
+router.get('/', checkAuth, ReminderController.showReminders);
 
 module.exports = router;
