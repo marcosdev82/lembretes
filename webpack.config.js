e1506aeb5f9379998feb8a1f448bbfc994aa730e
mikek182 <marcosvinicius@geekwork.com.br>
2025-06-17 16:51:34 -0300

    Modificação do entry point

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+        mode: 'development', // ou 'production' para builds finais
+        entry: './index.js',
+        output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true, // limpa a pasta dist a cada build
+    },
+    module: {
+    rules: [
+        {
+            test: /\.js$/, // aplica a regra para arquivos .js
+            exclude: /node_modules/,
+            use: {
+                loader: 'babel-loader', // se estiver usando Babel
+            },
+        },
+    ],
+    },
+    devtool: 'source-map', // útil para debug
+    devServer: {
+        static: './dist',
+        open: true,
+        port: 3000,
+    },
+};
