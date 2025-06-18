243aa0d337063fcca765c3f186947e22f66e3a98
mikek182 <marcosvinicius@geekwork.com.br>
2025-06-18 13:03:30 -0300

    variáveis de ambiente workflow

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const config = require('dotenv').config(); 
+require('dotenv').config();
 const express = require('express');
 const { engine } = require('express-handlebars');
 const session = require('express-session');
