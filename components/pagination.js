53c3e5fb53527384a30bdfe479fbc7a03b7093d7
mikek182 <marcosvinicius@geekwork.com.br>
2025-06-24 14:07:59 -0300

    param total render pagination

--- components/pagination.js
+++ components/pagination.js
@@ -1,25 +1,24 @@
-function renderPagination(currentPage, totalPages, search) {
+function renderPagination(currentPage, totalPages, total, search) {
 
-    if (totalPages >= 2) {
-     console.log(totalPages)
-        let paginationHtml = `<div class="flex gap-2 mt-4">`;
+    if (total >= 2) return;
+    
+    let paginationHtml = `<div class="flex gap-2 mt-4">`;
 
-        if (currentPage > 1) {
-            paginationHtml += `<a href="?page=${currentPage - 1}&search=${search}" class="px-3 py-1 bg-gray-200 rounded">Anterior</a>`;
-        }
-
-        for (let page = 1; page <= totalPages; page++) {
-            const activeClass = (page === currentPage) ? 'bg-blue-500 text-white' : 'bg-gray-200';
-            paginationHtml += `<a href="?page=${page}&search=${search}" class="px-3 py-1 ${activeClass} rounded">${page}</a>`;
-        }
-
-        if (currentPage < totalPages) {
-            paginationHtml += `<a href="?page=${currentPage + 1}&search=${search}" class="px-3 py-1 bg-gray-200 rounded">Próximo</a>`;
-        }
-
-        paginationHtml += `</div>`;
-        return paginationHtml;
+    if (currentPage > 1) {
+        paginationHtml += `<a href="?page=${currentPage - 1}&search=${search}" class="px-3 py-1 bg-gray-200 rounded">Anterior</a>`;
     }
+
+    for (let page = 1; page <= totalPages; page++) {
+        const activeClass = (page === currentPage) ? 'bg-blue-500 text-white' : 'bg-gray-200';
+        paginationHtml += `<a href="?page=${page}&search=${search}" class="px-3 py-1 ${activeClass} rounded">${page}</a>`;
+    }
+
+    if (currentPage < totalPages) {
+        paginationHtml += `<a href="?page=${currentPage + 1}&search=${search}" class="px-3 py-1 bg-gray-200 rounded">Próximo</a>`;
+    }
+
+    paginationHtml += `</div>`;
+    return paginationHtml;
 }
 
 module.exports = renderPagination; 
