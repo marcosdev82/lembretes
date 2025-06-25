c49a5db090c16c679dd5fb228f3dc69812aa42fe
mikek182 <marcosvinicius@geekwork.com.br>
2025-06-25 17:26:28 -0300

    Ajuste desfeito

--- components/pagination.js
+++ components/pagination.js
@@ -10,7 +10,7 @@ function renderPagination(currentPage, totalPages, showPatination, search) {
 
         for (let page = 1; page <= totalPages; page++) {
             const activeClass = (page === currentPage) ? 'bg-blue-500 text-white' : 'bg-gray-200';
-            paginationHtml += `<span "?page=${page}&search=${search}" class="px-3 py-1 ${activeClass} rounded">${page}</span>`;
+            paginationHtml += `<a href="?page=${page}&search=${search}" class="px-3 py-1 ${activeClass} rounded">${page}</a>`;
         }
 
         if (currentPage < totalPages) {
