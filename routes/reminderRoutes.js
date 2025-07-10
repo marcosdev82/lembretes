3f2f19484cda2491ca7dc050fc43077d968984c6
mikek182 <marcosvinicius@geekwork.com.br>
2025-07-10 12:18:06 -0300

    Rota remove to trash

--- routes/reminderRoutes.js
+++ routes/reminderRoutes.js
@@ -9,11 +9,11 @@ const checkAuth = require('../helpers/auth').checkAuth;
 // Rotas
 router.get('/dashboard', checkAuth, ReminderController.dashboard);
 router.get('/home', checkAuth, ReminderController.showReminders);
-router.get('/add', checkAuth, ReminderController.createReminder); // Exibe o formulário
-router.post('/add', checkAuth, ReminderController.createReminderSave); // Salva o formulário 
-router.get('/edit/:id', checkAuth, ReminderController.updateReminder)
-router.post('/edit/:id', checkAuth, ReminderController.updateReminderSave)
-router.post('/remove', checkAuth, ReminderController.removeReminder)
+router.get('/add', checkAuth, ReminderController.createReminder); 
+router.post('/add', checkAuth, ReminderController.createReminderSave); 
+router.get('/edit/:id', checkAuth, ReminderController.updateReminder);
+router.post('/edit/:id', checkAuth, ReminderController.updateReminderSave);
+router.post('/remove', checkAuth, ReminderController.moveToTrash);
 router.get('/', checkAuth, ReminderController.showReminders);
 
 module.exports = router;
